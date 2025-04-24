e0f6333de9782d1d08d3212a5ec69f37ed979e68
MarqAlejandro <alejandromarq3@gmail.com>
2025-04-24 08:41:23 -0700

    Updated with commentary

--- FormatDates/src/main/java/com/plurasight/FormatDate.java
+++ FormatDates/src/main/java/com/plurasight/FormatDate.java
@@ -8,16 +8,16 @@ import java.time.format.DateTimeFormatter;
 
 public class FormatDate {
 
-    private final LocalDate TODAYDATE = LocalDate.now();
+    private final LocalDate TODAYDATE = LocalDate.now();                                            //local date and time variables
     private final LocalTime TODAYTIME = LocalTime.now();
-    private final LocalTime TODAYTIMEGMT = LocalTime.now().plusHours(7);
+    private final LocalTime TODAYTIMEGMT = LocalTime.now().plusHours(7);                 //local time in Greenwich Mean Time
     private final LocalDateTime TODAYDATETIME = LocalDateTime.now();
-    private DateTimeFormatter dateFormatter;
+    private DateTimeFormatter dateFormatter;                                                        //formatter variables
     private DateTimeFormatter timeFormatter;
-    private String formatDate;
+    private String formatDate;                                                                      //Strings to hold the formatted dates and times
     private String formatTime;
 
-    public void displayDifferentFormats(){
+    public void displayDifferentFormats(){                                                          //main control method calls other methods in order
         FormatTodayDateAsMM_dd_yyyy();
         FormatTodayDateAsISO_8601();
         FormatTodayDateAsMonth_dd_yyyy();
@@ -27,7 +27,7 @@ public class FormatDate {
         FormatTodayDateTimeAsHHmm_on_dd_MMM_yyyy();
     }
 
-    public void FormatTodayDateAsMM_dd_yyyy(){
+    public void FormatTodayDateAsMM_dd_yyyy(){                                                      //displays date as sample: 01/01/2025
 
         dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         formatDate = TODAYDATETIME.format(dateFormatter);
@@ -35,12 +35,12 @@ public class FormatDate {
         System.out.println(formatDate);
     }
 
-    public void FormatTodayDateAsISO_8601(){
+    public void FormatTodayDateAsISO_8601(){                                                       //displays date as sample: 2025-01-01
 
         System.out.println(TODAYDATE);
     }
 
-    public void FormatTodayDateAsMonth_dd_yyyy(){
+    public void FormatTodayDateAsMonth_dd_yyyy(){                                                  //displays date as sample: Jan 1, 2025
 
         dateFormatter = DateTimeFormatter.ofPattern("MMM dd, yyyy");
         formatDate = TODAYDATE.format(dateFormatter);
@@ -49,7 +49,7 @@ public class FormatDate {
 
     }
 
-    public void FormatTodayDateTimeAsE_MMM_dd_yyyy_HHmm_in_GMT(){
+    public void FormatTodayDateTimeAsE_MMM_dd_yyyy_HHmm_in_GMT(){                                  //display date and time as sample: Wed, Jan 1, 2025 10:01 AM
 
         dateFormatter = DateTimeFormatter.ofPattern("E, MMM dd, yyyy");
         formatDate = TODAYDATETIME.format(dateFormatter);
@@ -62,7 +62,7 @@ public class FormatDate {
         System.out.println(formatDate + "  " + formatTime);
     }
 
-    public void FormatTodayDateTimeAsHHmm_on_dd_MMM_yyyy(){
+    public void FormatTodayDateTimeAsHHmm_on_dd_MMM_yyyy(){                                        //display time and date as sample: 10:01 AM on 01-Jan-2025
 
         dateFormatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
         formatDate = TODAYDATETIME.format(dateFormatter);
